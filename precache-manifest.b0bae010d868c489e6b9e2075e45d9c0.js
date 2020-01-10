e7583c4581b9dcd66ae31fa2bc33c62c1143bab7
Anthony <anthony@Apples-MacBook-Pro.local>
2020-01-10 14:30:27 -0500

    Updates

---
+++ precache-manifest.b0bae010d868c489e6b9e2075e45d9c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1369f86f3fee05ad53a406bb3be54912",
+    "url": "/cuny2x-colorgrid/index.html"
+  },
+  {
+    "revision": "e0443cb7e619afe32c0b",
+    "url": "/cuny2x-colorgrid/static/css/main.d966042f.chunk.css"
+  },
+  {
+    "revision": "2e105ac113e77c53bdbb",
+    "url": "/cuny2x-colorgrid/static/js/2.8f66ba17.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cuny2x-colorgrid/static/js/2.8f66ba17.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0443cb7e619afe32c0b",
+    "url": "/cuny2x-colorgrid/static/js/main.c0ca2a15.chunk.js"
+  },
+  {
+    "revision": "518a6590f7b68a8aac95",
+    "url": "/cuny2x-colorgrid/static/js/runtime-main.db42886e.js"
+  }
+]);
